35a3360b6105e0f22c1f5d2c2f7436d4f77dc87c
TheRealRemo <timothyprice2001@gmail.com>
2026-04-14 09:14:27 -0400

    Added spaceIndex for space between first and lastName and implemented it to set up "lastName, firstName" format

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -2,17 +2,27 @@ package com.pluralsight;
 
 import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+
+import static javax.print.attribute.Size2DSyntax.MM;
 
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Welcome to Theater Reservations! Please enter your name: ");
         String name = myScanner.nextLine();
-        System.out.println("Please enter your DOB, (MM/DD/YYYY): ");
+        System.out.println("Please enter your DOB, (MM/dd/yyyy): ");
         String dateOfBirth = myScanner.nextLine();
         System.out.println("How many ticket(s) would you like?: ");
         int tickets = myScanner.nextInt();
+
+        int spaceIndex = name.indexOf(" ");
+        String firstName = name.substring(0, spaceIndex);
+        String lastName = name.substring(spaceIndex + 1);
+
+        System.out.println(lastName + ", " + firstName);
 
 
     }
