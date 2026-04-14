bbaccb3d09cc14c2c2761f4c1e9dd4d4e54b41ff
TheRealRemo <timothyprice2001@gmail.com>
2026-04-14 11:00:34 -0400

    Added print out for input prompts

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -8,21 +8,22 @@ import java.util.Scanner;
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Welcome to Theater Reservations! Please enter your name: ");
+        System.out.print("Welcome to Theater Reservations! Please enter your name: ");
         String name = myScanner.nextLine();
-        System.out.println("Please enter your DOB, (MM/dd/yyyy): ");
-        String dateOfBirth = myScanner.nextLine();
-        System.out.println("How many ticket(s) would you like?: ");
+        System.out.print("Please enter your booking date, (MM/dd/yyyy): ");
+        String bookingDate = myScanner.nextLine();
+        System.out.print("How many ticket(s) would you like?: ");
         int tickets = myScanner.nextInt();
 
         int spaceIndex = name.indexOf(" ");
         String firstName = name.substring(0, spaceIndex);
         String lastName = name.substring(spaceIndex + 1);
 
-        System.out.println(lastName + ", " + firstName);
+        ;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        LocalDate date = LocalDate.parse(dateOfBirth, formatter);
-        System.out.println(date);
-
+        LocalDate date = LocalDate.parse(bookingDate, formatter);
+        System.out.println(tickets +" tickets reserved for " + date + " under " + lastName + ", " + firstName);
+        myScanner.close();
     }
 }
+
